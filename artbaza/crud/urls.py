1f15e3b239ea0944637dcc8437df5f8160cb498c
George Lir <heraklakra@gmail.com>
2025-01-30 13:40:08 +0500

    Changed main page of crud. Added create and delete operations.

--- artbaza/crud/urls.py
+++ artbaza/crud/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('delete/<str:model_name>/<int:pk>/', views.delete_record, name='delete_record'),
+    path('add/<str:model_name>/', views.add_record, name='add_record'),
+    path('form/<str:model_name>/<int:pk>/', views.form, name='form'),
 ]
--- artbaza/crud/views.py
+++ artbaza/crud/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.apps import apps
-from django.http import HttpResponseNotAllowed
+from django.http import HttpResponseNotAllowed, HttpResponseBadRequest
 from .models import Author
 
 def index(request):
@@ -17,3 +17,34 @@ def delete_record(request, model_name, pk):
         record.delete()
         return redirect('/crud/')
     return HttpResponseNotAllowed(['POST'])
+
+def add_record(request, model_name):
+    if request.method == "POST":
+        model = apps.get_model(app_label='crud', model_name=model_name)
+        if not model:
+            return HttpResponseNotAllowed(['POST'], "Модель не найдена")
+        fields = {}
+        for field in model._meta.fields:
+            if field.name in request.POST:
+                value = request.POST.get(field.name)
+                # Обрабатываем пустую строку для числовых и других nullable полей
+                if value == '' and (field.null or field.blank):
+                    value = None
+                fields[field.name] = value
+        try:
+            record = model.objects.create(**fields)
+        except Exception as e:
+            return HttpResponseBadRequest(f"Ошибка: {str(e)}")
+        return redirect('/crud/')
+    return HttpResponseNotAllowed(['POST'])
+
+def form(request, model_name, pk):
+    if request.method == "GET":
+        model = apps.get_model(app_label='crud', model_name=model_name)
+        if not model:
+            return HttpResponseNotAllowed(['GET'], "Модель не найдена")
+        if pk != -1:
+            record = get_object_or_404(model, pk=pk)
+        else:
+            pass
+    return  render(request, 'form.html')
